6a72c1147b13eacce21c910e7417850942905255
sujonifty <sujonifty@gmail.com>
2024-04-29 11:12:51 +0600

    completed sorting

--- index.js
+++ index.js
@@ -9,8 +9,8 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.b9hcdyj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -26,12 +26,14 @@ async function run() {
     
 
         const spotCollection = client.db('globifyDB').collection('touristSpot');
+        const tourismCollection = client.db('globifyDB').collection('tourism');
         app.post('/touristSpot', async (req, res) => {
             const newSpot = req.body;
             console.log(newSpot);
             const result = await spotCollection.insertOne(newSpot);
             res.send(result);
         })
+       
 
         // all tourist spot 
         app.get('/touristSpot', async (req, res) => {
@@ -41,8 +43,12 @@ async function run() {
         })
 
         app.get('/touristSort', async (req, res) => {
-            // const query = { cost: price };
-            const cursor = spotCollection.find(query, options).sort({ cost: 1 });
+            const cursor = spotCollection.find().sort({ cost: 1 });
+            const result = await cursor.toArray();
+            res.send(result);
+        })
+        app.get('/sort', async (req, res) => {
+            const cursor = spotCollection.find().sort({ cost: 0 });
             const result = await cursor.toArray();
             res.send(result);
         })
